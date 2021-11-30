98acee42c6e21c2c56df861958a6ebcf80f03199
Melissa Amaro <amaro.melissa13@gmail.com>
2021-11-30 08:48:17 -0800

    calculator

---
+++ programcal/Calculator.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Create a dotnet console application to emulate a calculator.
+static void Main()
+{
+char Operators;
+double fun1, fun2, fun3;
+
+Console.WriteLine("Choose Operation method (+, -, *, /)");
+Operators = Console.ReadLine()[0];
+
+Console.WriteLine("Enter the three number values to calculate");
+fun1 = Convert.ToDouble(Console.ReadLine());
+fun2 = Convert.ToDouble(Console.ReadLine());
+
+
+    switch(Operators)
+    {
+        case '+':
+            Console.WriteLine("{0} + {1} = {2}", fun1, fun2,(fun1 + fun2));
+            break;
+
+        case '-':
+            Console.WriteLine("{0} - {1} = {2}", fun1, fun2, (fun1 - fun2));
+            break;
+
+        case '*':
+            Console.WriteLine.("{0} * {1} = {2}", fun1, fun2, (fun1 * fun2));
+            break;
+        
+        case '/':
+            Console.WriteLine("{0} / {1} = {2}", fun1, fun2, (fun1 / fun2));
+            break;
+        Default:
+            Console.WriteLine("Not a valid operation")
+            break;
+
+    }
+While();   
+Console.WriteLine("Press any key to exit the calculator");
+Console.ReadKey();
+
+}
+Main()
+
+
+//Your application should (mvp):
+//-Be able to accept multiple numbers
+//-Perform a selected operation on those numbers
+//-Display the result of the operation
+//-Repeat until the user chooses to exit
